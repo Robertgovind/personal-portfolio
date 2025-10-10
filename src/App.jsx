e8e47db5099de48ae296720f1c16f7848bd38e43
Goivnd Kumar Yadav <robertgovind@gmail.com>
2025-10-10 14:09:44 +0545

    adding build file

--- src/App.jsx
+++ src/App.jsx
@@ -12,9 +12,12 @@ import Certifications from "./pages/Certifications.jsx";
 function App() {
   return (
     <div className="min-h-screen flex flex-col text-white relative overflow-hidden">
-      <div className="absolute inset-0 -z-10 bg-gradient-to-br from-gray-900 via-[#0d1b2a] to-[#000814]"></div>
-      <div className="absolute top-0 left-0 w-[500px] h-[500px] bg-blue-500/20 rounded-full blur-3xl animate-pulse -z-10"></div>
-      <div className="absolute bottom-0 right-0 w-[500px] h-[500px] bg-purple-600/20 rounded-full blur-3xl animate-pulse delay-700 -z-10"></div>
+      <div className="absolute inset-0 -z-10 bg-gradient-to-br
+       from-gray-900 via-[#0d1b2a] to-[#000814]"></div>
+      <div className="absolute top-0 left-0 w-[500px] h-[500px]
+       bg-blue-500/20 rounded-full blur-3xl animate-pulse -z-10"></div>
+      <div className="absolute bottom-0 right-0 w-[500px] h-[500px]
+       bg-purple-600/20 rounded-full blur-3xl animate-pulse delay-700 -z-10"></div>
 
   
       <Navbar />
